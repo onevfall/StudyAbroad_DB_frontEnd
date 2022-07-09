f0c1523c59a1ea633ac8d33beb08be4cb18fe176
JiaoJiao <923252409@qq.com>
2022-07-09 22:50:36 +0800

    更改导航栏标签；动态详情引入相关动态组件
    下一步完成相关动态组件封装、实现动态详情布局

---
+++ src/components/BlogInfoBoard.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card class="box-card blog_card" shadow="hover" >
+    <template #header>
+     
+    </template>
+    
+  </el-card>
+</template>
+
+<script>
+//到时候传入一个
+export default {
+ 
+  
+  methods:{
+      
+  }
+};
+</script>
+
+<style scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #299afb;
+  font-weight: bolder;
+  margin-bottom: 10px;
+}
+.blog_card {
+  width: 350px;
+}
+
+</style>
