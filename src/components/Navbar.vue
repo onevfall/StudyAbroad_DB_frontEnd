63d646f493fdb385c260bcbb9804bdf8131538de
FangXinyu-0913 <fangxinyutju202009@126.com>
2022-07-14 14:23:08 +0800

    小修改

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -19,7 +19,7 @@
     <el-menu-item index="school_center" @click="goSchoolCenter">找对学校</el-menu-item>
     <el-sub-menu index="3">
       <template #title>说说留学</template>
-      <el-menu-item index="answer_detail" @click="goQuestion">留学问答(测试用 跳转至问题详情界面)</el-menu-item>
+      <el-menu-item index="question" @click="goQuestion">留学问答(测试用 跳转至问题详情界面)</el-menu-item>
       <el-menu-item index="blog" @click="goBlog">动态分享</el-menu-item>
     </el-sub-menu>
     <el-menu-item index="news" @click="goNewsHome">留学快讯</el-menu-item>
@@ -40,7 +40,6 @@
     </el-sub-menu>
     <el-menu-item index="school_detail" @click="goSchoolDetail">学校详情</el-menu-item>
     <el-menu-item index="institution_detail" @click="goInstitutionDetail">机构详情</el-menu-item>
-    <el-menu-item index="question" @click="goQuestion">问题</el-menu-item>
   </el-menu>
 </template>
 
