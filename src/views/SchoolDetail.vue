71e4ada1e8b80e9afbb33454ed786be290c5f8d1
Bestom927 <2480518677@qq.com>
2022-07-15 10:13:19 +0800

    广告栏覆盖

--- src/views/SchoolDetail.vue
+++ src/views/SchoolDetail.vue
@@ -8,7 +8,19 @@
     <school-info :school ="this.school_info"></school-info>
     <el-container>
       <el-aside width=25%>
-        
+        <el-card
+                  :body-style="{ background: 'aliceblue' }"
+                  shadow="always"
+                >
+                  <template #header>
+                    <div class="card-header">广告栏</div>
+                  </template>
+                  <div v-for="link in links" :key="link.text" class="text">
+                    <el-link href="https://sse.tongji.edu.cn">
+                      {{ link.text }}</el-link
+                    >
+                  </div>
+        </el-card>  
       </el-aside>
       <el-main>
         <div class="info_detail">
@@ -35,6 +47,14 @@ export default {
     return {
       school_info: "",
       school_id:"",
+      links: [
+        { text: "同济软院招聘信息" },
+        { text: "同济经管学院招聘信息" },
+        { text: "同济电院招聘信息" },
+        { text: "同济土木学院招聘信息" },
+        { text: "同济数院招聘信息" },
+        { text: "同济物院招聘信息" },
+      ],
     };
   },
   props:["school_id"],
@@ -70,5 +90,18 @@ export default {
 </script>
 
 <style scoped>
+.text {
+  font-size: 14px;
+  text-align: left;
 
+  margin-bottom: 18px;
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+  font-size: 28px;
+  padding-left: 5px;
+  border-left: 10px rgb(33, 33, 126) solid;
+}
 </style>
